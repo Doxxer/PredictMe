2758f6d2e467c9464437ba1051e24ad3319be16e
Alprobit <alprobit@gmail.com>
2014-11-06 17:07:27 +0300

    Computing movie rating

--- neurotest/tests/datasets.py
+++ neurotest/tests/datasets.py
@@ -2,20 +2,22 @@
 
 import MySQLdb
 import pickle
-from pybrain.datasets import SupervisedDataSet
-
-
 
 
 db = MySQLdb.connect(host="localhost"
-                     , user="root"
-                     , passwd="@Q:27182;Byte"
+                     , user="imdb2"
+                     , passwd="imdb2"
                      , db="imdb2")
-
 movies_cur = db.cursor()
+
+movies_cur.execute("DROP TABLE IF EXISTS RatedActors;")
 movies_cur.execute("DROP TABLE IF EXISTS RatedDirectors;")
 movies_cur.execute("DROP TABLE IF EXISTS RatedProducers;")
 
+movies_cur.execute("""CREATE TABLE RatedActors AS
+                    select person_id, movie_id, AVG(rating) as rating
+                    from ActorsFULL
+                    GROUP BY person_id;""")
 movies_cur.execute("""CREATE TABLE RatedDirectors AS
                     select person_id, movie_id, AVG(rating) as rating
                     from DirectorFULL
@@ -25,27 +27,13 @@ movies_cur.execute("""CREATE TABLE RatedProducers AS
                     from ProducerFULL
                     GROUP BY person_id;""")
 
-actors_cur = db.cursor()
-directors_cur = db.cursor()
-producers_cur = db.cursor()
-
-
-for actors_num in [1, 2, 3, 4, 5]:
-    print actors_num
-
-    dataset = SupervisedDataSet(actors_num, 1)
-    movies_cur.execute("DROP TABLE IF EXISTS RatedActors;")
-
-    movies_cur.execute("""CREATE TABLE RatedActors AS
-                        select person_id, movie_id, AVG(rating) as rating
-                        from ActorsFULL
-                        GROUP BY person_id
-                        HAVING COUNT(person_id) >= {0};""".format(actors_num))
-
+datasets = []
+for i in range(6):
+    print i
+    datasets.append([])
     movies_cur.execute("""select movie_id, rating, year from  allMovies
                         where movie_id in (
                           select movie_id from ActorsFULL
-                          where person_id in (select person_id from RatedActors)
                         ) AND  movie_id in (
                           select movie_id from DirectorFULL
                         ) AND movie_id in (
@@ -54,11 +42,14 @@ for actors_num in [1, 2, 3, 4, 5]:
                         ORDER BY RAND()
                         limit 100""")
 
+    actors_cur = db.cursor()
+    directors_cur = db.cursor()
+    producers_cur = db.cursor()
     for movie in movies_cur:
         actors_cur.execute("""select rating from RatedActors
                               where person_id in (
                                  select person_id from ActorsFULL where movie_id = {0} )
-                              ORDER BY rating desc limit {1}""".format(movie[0], actors_num))
+                              ORDER BY rating desc limit 1""".format(movie[0]))
         directors_cur.execute("""select rating from RatedDirectors
                               where person_id in (
                                  select person_id from DirectorFULL where movie_id = {0} )
@@ -67,16 +58,12 @@ for actors_num in [1, 2, 3, 4, 5]:
                               where person_id in (
                                  select person_id from ProducerFULL where movie_id = {0} )
                               ORDER BY rating desc limit 1""".format(movie[0]))
-
-        actors_rates = tuple(actors_cur.fetchmany(actors_num)[0])
-        print actors_rates
-        dataset.addSample((movie[2],) + actors_rates +
-                          (producers_cur.fetchone()[0], directors_cur.fetchone()[0],), (movie[1],))
-
-    with open('dataset_' + actors_num + '.data', 'wb') as f:
-        pickle.dump(dataset, f)
+        datasets[i].append([(movie[2], actors_cur.fetchone()[0], directors_cur.fetchone()[0], producers_cur.fetchone()[0],), (movie[1],)])
 
 movies_cur.close()
 actors_cur.close()
 directors_cur.close()
 producers_cur.close()
+
+with open('datasets.pickle', 'wb') as f:
+    pickle.dump(datasets, f)
