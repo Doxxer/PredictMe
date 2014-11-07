1fa77d6dc305684c6f2b8dd73b637b4e781755fd
Alprobit <alprobit@gmail.com>
2014-11-07 16:30:01 +0300

    neuronets was fixed

--- AppServer/PredictMe/neurnets/neurnets.py
+++ AppServer/PredictMe/neurnets/neurnets.py
@@ -8,6 +8,55 @@ import pickle
 from PredictMe.ratingExtractor.ratingExtractor import get_rating
 
 
+def fill_subdataset(db, movies_cur, actors_cur, directors_cur, writers_cur, actors_num, dataset, minrate, maxrate):
+    movies_cur.execute("""select distinct(title_id), allMovies.rating, allMovies.production_year
+                            from  allMovies
+                            where rating >= {0} and rating <= {1} and
+                            exists  (
+                              select title_id from PersonsFULL
+                              where (role_id = 1 or role_id = 2) and PersonsFULL.title_id = allMovies.title_id
+                              group by title_id
+                              having count(person_id) >= {2}
+                            ) and exists (
+                              select title_id from PersonsFULL
+                              where role_id = 8 and PersonsFULL.title_id = allMovies.title_id
+                            ) and exists (
+                              select title_id from PersonsFULL
+                              where role_id = 4 and PersonsFULL.title_id = allMovies.title_id
+                            )
+                            ORDER BY RAND()
+                            LIMIT 25""".format(minrate, maxrate, actors_num))
+
+    for movie in movies_cur:
+        actors_cur.execute("""select ratedActors.actor_rating
+                              from PersonsFULL inner join ratedActors
+                                on PersonsFULL.person_id = ratedActors.person_id
+                                  and (PersonsFULL.role_id = 1 or PersonsFULL.role_id = 2)
+                                  and title_id = {0}
+                              group by ratedActors.person_id
+                              order by RAND() limit {1}""".format(movie[0], actors_num))
+        directors_cur.execute("""select ratedDirectors.director_rating
+                              from PersonsFULL inner join ratedDirectors
+                                on PersonsFULL.person_id = ratedDirectors.person_id
+                                  and PersonsFULL.role_id = 8
+                                  and title_id = {0}
+                              group by ratedDirectors.person_id
+                              order by RAND() limit 1""".format(movie[0]))
+        writers_cur.execute("""select ratedWriters.writer_rating
+                              from PersonsFULL inner join ratedWriters
+                                on PersonsFULL.person_id = ratedWriters.person_id
+                                  and PersonsFULL.role_id = 4
+                                  and title_id = {0}
+                              group by ratedWriters.person_id
+                              order by RAND() limit 1""".format(movie[0]))
+
+        actors_rates = tuple(t[0] for t in actors_cur.fetchmany(actors_num))
+        dataset.addSample(# (movie[2],) +
+                          actors_rates +
+                          (writers_cur.fetchone()[0], directors_cur.fetchone()[0],), (movie[1],))
+
+
+
 def create_datasets():
     db = MySQLdb.connect(host="localhost",
                          user="imdb",
@@ -19,57 +68,14 @@ def create_datasets():
     directors_cur = db.cursor()
     writers_cur = db.cursor()
 
-    for actors_num in range(2, 6):
+    for actors_num in range(1, 6):
         print actors_num
 
-        dataset = SupervisedDataSet(3 + actors_num, 1)
-
-        movies_cur.execute("""select distinct(title_id), allMovies.rating, allMovies.production_year
-                            from  allMovies
-                            where exists  (
-                              select title_id from PersonsFULL
-                              where (role_id = 1 or role_id = 2) and PersonsFULL.title_id = allMovies.title_id
-                              group by person_id, title_id
-                              having count(person_id) >= {0}
-                            ) and exists (
-                              select title_id from PersonsFULL
-                              where role_id = 8 and PersonsFULL.title_id = allMovies.title_id
-                            ) and exists (
-                              select title_id from PersonsFULL
-                              where role_id = 4 and PersonsFULL.title_id = allMovies.title_id
-                            )
-                            ORDER BY RAND()
-                            LIMIT 100""".format(actors_num))
-
-        for movie in movies_cur:
-            actors_cur.execute("""select ratedActors.actor_rating
-                                  from PersonsFULL inner join ratedActors
-                                    on PersonsFULL.person_id = ratedActors.person_id
-                                      and (PersonsFULL.role_id = 1 or PersonsFULL.role_id = 2)
-                                      and title_id = {0}
-                                  group by ratedActors.person_id
-                                  order by RAND() limit {1}""".format(movie[0], actors_num))
-            directors_cur.execute("""select ratedDirectors.director_rating
-                                  from PersonsFULL inner join ratedDirectors
-                                    on PersonsFULL.person_id = ratedDirectors.person_id
-                                      and PersonsFULL.role_id = 8
-                                      and title_id = {0}
-                                  group by ratedDirectors.person_id
-                                  order by RAND() limit 1""".format(movie[0]))
-            writers_cur.execute("""select ratedWriters.writer_rating
-                                  from PersonsFULL inner join ratedWriters
-                                    on PersonsFULL.person_id = ratedWriters.person_id
-                                      and PersonsFULL.role_id = 4
-                                      and title_id = {0}
-                                  group by ratedWriters.person_id
-                                  order by RAND() limit 1""".format(movie[0]))
-
-            actors_rates = tuple(t[0] for t in actors_cur.fetchmany(actors_num))
-            try:
-                dataset.addSample((movie[2],) + actors_rates +
-                                  (writers_cur.fetchone()[0], directors_cur.fetchone()[0],), (movie[1],))
-            except ValueError:
-                print movie, actors_rates, actors_num
+        dataset = SupervisedDataSet(2 + actors_num, 1)
+        fill_subdataset(db, movies_cur, actors_cur, directors_cur, writers_cur, actors_num, dataset, 0, 2)
+        fill_subdataset(db, movies_cur, actors_cur, directors_cur, writers_cur, actors_num, dataset, 2, 4.2)
+        fill_subdataset(db, movies_cur, actors_cur, directors_cur, writers_cur, actors_num, dataset, 4.2, 8.6)
+        fill_subdataset(db, movies_cur, actors_cur, directors_cur, writers_cur, actors_num, dataset, 8.6, 10)
 
         with open('dataset_' + str(actors_num) + '.data', 'wb') as f:
             pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
@@ -83,7 +89,7 @@ def create_datasets():
 def trainNetwork(dataset, dim):
     net = buildNetwork(3 + dim, 3 + dim, 1)
     trainer = BackpropTrainer(net, dataset)
-    trainer.trainEpochs(4)
+    trainer.trainEpochs(10)
     return net
 
 
@@ -113,3 +119,5 @@ def computeMovieRating(movie_year, actors, writers, directors):
 
 if __name__ == "__main__":
     print computeMovieRating(0, 0, 0, 0)
+
+
