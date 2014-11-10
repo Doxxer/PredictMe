5e3f6543eb262d0ddbc4de9f1602ccf2e2d7950e
katejim <katejimme@gmail.com>
2014-11-10 16:38:02 +0300

    Add coefficients from R to Python

--- regrassions/regression.py
+++ regrassions/regression.py
@@ -4,36 +4,12 @@ import rpy
 import pandas
 
 #format data - (ActorRating<2.5, ActorRating<5, ActorRating<7.5, ActorRating<10, DirectorRating, WriterRating)
+#coefficients from R
 def predictRating(film_data):
-    data = pandas.read_csv("DataRegression.csv")
-    rpy.set_default_mode(rpy.NO_CONVERSION)
-    linear_model = rpy.r.lm(rpy.r("Rating ~ ."), data = rpy.r.data_frame(Rating=data['Rating'],
-                                                                     Director=data['Director'],
-                                                                     Writer=data['Writer'],
-                                                                     Actor1=data['Actor.Group1'],
-                                                                     Actor2=data['Actor.Group2'],
-                                                                     Actor3=data['Actor.Group3'],
-                                                                     Actor4=data['Actor.Group4']))
-
-    predicted = rpy.r.predict(linear_model, rpy.r.data_frame(Actor1=film_data[0],
-                                                         Actor2=film_data[1],
-                                                         Actor3=film_data[2],
-                                                         Actor4=film_data[3],
-                                                         Director=film_data[4],
-                                                         Writer=film_data[5]))
-
-
-
-
-
-    rpy.set_default_mode(rpy.BASIC_CONVERSION)
-    # print linear_model.as_py()['coefficients']
-    # print predicted.as_py()
-    # print data['Rating'][0]
-    return predicted.as_py()['1']
-
-
-
+    predicted_value = 0.0882967271 - 0.0120020289 * film_data[0] - 0.0106449352 * film_data[1] +  0.0003242131 * film_data[2] +
+        0.0117709794 * film_data[3] + 0.3486407973 * film_data[4] + 0.6362357986 * film_data[5]
+    return predicted_value
+    
 if __name__ == '__main__' :
     data = pandas.read_csv("DataRegression.csv")
     data_test = (data['Actor.Group1'][0], data['Actor.Group2'][0], data['Actor.Group3'][0], data['Actor.Group4'][0], data['Director'][0], data['Writer'][0])
